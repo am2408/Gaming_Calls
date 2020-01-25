79bf126368b458f4355ed1f7d060cc6955f29c9c
Abdoul <abdoul.m2408@gmail.com>
2020-01-25 13:07:12 +0100

    Secure

--- application/controllers/basket/BasketController.class.php
+++ application/controllers/basket/BasketController.class.php
@@ -4,6 +4,9 @@ class BasketController
 {
     public function httpGetMethod(Http $http, array $queryFields)
     {
+      if(empty($_SESSION) == true) {
+        $http->redirectTo('/');
+      }
     }
 
     public function httpPostMethod(Http $http, array $formFields)
